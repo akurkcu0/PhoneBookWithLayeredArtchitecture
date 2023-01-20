aecd34d3671ca339e369dfc5e754b7949b7ae5bc
akurkcu0 <kurkcuabdurrahman@hotmail.com>
2023-01-20 11:24:42 +0300

    Raporlama servisi eklendi

---
+++ Business/Concrete/ReportManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using Core.Utilities.Results.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ReportManager : IReportService
+    {
+        IReportsDal _reportDal;
+
+        public ReportManager(IReportsDal reportDal)
+        {
+            _reportDal = reportDal;
+        }
+
+        public IDataResult<List<Report>> GetByLocations()
+        {
+            return new SuccesDataResult<List<Report>>(_reportDal.GetByLocations());
+        }
+    }
+}
