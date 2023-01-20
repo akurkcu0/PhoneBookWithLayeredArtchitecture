ca4c64a80c462a501ab4342dc03302cc07e3e87e
akurkcu0 <kurkcuabdurrahman@hotmail.com>
2023-01-20 12:04:50 +0300

    Rapor için API controller yapıldı.

---
+++ PhoneBookWenApi/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneBookWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+
+        [HttpGet("getByLocation")]
+        public IActionResult GetByLocationAll()
+        {
+            var result = _reportService.GetByLocations();
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
