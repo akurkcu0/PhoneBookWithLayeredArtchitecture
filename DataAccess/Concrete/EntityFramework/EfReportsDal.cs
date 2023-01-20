e2f3f84f6775400c73fe6ba0f83fb45d430b5ab8
akurkcu0 <kurkcuabdurrahman@hotmail.com>
2023-01-20 11:14:50 +0300

    Rapor için entity eklendi ve LINQ kullanılarak gerekli srogu yazıldı

---
+++ DataAccess/Concrete/EntityFramework/EfReportsDal.cs
@@ -0,0 +1,34 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Context;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfReportsDal : EfEntityRepositoryBase<ContactInformation, PhoneBookDbContext>, IReportsDal
+    {
+        public List<Report> GetByLocations()
+        {
+            using (PhoneBookDbContext context = new PhoneBookDbContext())
+            {
+                var locations = context.contactInformations.GroupBy(p => p.Location).Select(s => s.Key).ToList();
+                var result = from location in locations
+                             select new Report()
+                             {
+                                 Location = location,
+                                 NumberOfPeople = context.contactInformations.Where(p => p.Location == location).Count(),
+                                 NumberOfPhone = context.contactInformations.Where(p => p.Location == location && p.PhoneNumber != "").Count(),
+                             };
+
+                return result.OrderBy(p => p.NumberOfPeople).ToList();
+
+            }
+        }
+    }
+}
---
+++ Entities/Concrete/Report.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Report
+    {
+        public string Location { get; set; }
+        public int NumberOfPeople { get; set; }
+        public int NumberOfPhone { get; set; }
+    }
+}
