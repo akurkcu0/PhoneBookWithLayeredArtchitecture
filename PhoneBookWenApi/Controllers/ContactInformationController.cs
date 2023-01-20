62bca28b9611d2e418332e39fa1c0356e77163c2
akurkcu0 <kurkcuabdurrahman@hotmail.com>
2023-01-20 12:00:57 +0300

    İletişim bilgileri API controller yapıldı

---
+++ PhoneBookWenApi/Controllers/ContactInformationController.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneBookWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactInformationController : ControllerBase
+    {
+        private readonly IContactInformationService _contactInformationService;
+            public ContactInformationController(IContactInformationService contactInformationService)
+        {
+            _contactInformationService = contactInformationService;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(ContactInformation contactInformation)
+        {
+            if (contactInformation == null) return BadRequest();
+
+            var result = _contactInformationService.Add(contactInformation);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete(ContactInformation contactInformation)
+        {
+            var result = _contactInformationService.Delete(contactInformation);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
